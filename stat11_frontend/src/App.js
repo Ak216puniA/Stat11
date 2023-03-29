e7c33b9a401a24e6a4f6a888a3cd0402b932010d
manashree635315 <kalodelaxmikant38@gmail.com>
2023-03-29 23:07:00 +0530

    Match Card UI updated

--- stat11_frontend/src/App.js
+++ stat11_frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Auth from './pages/auth';
+import MediaCard from './components/cards/card';
 
 function App() {
   return (
@@ -19,6 +20,15 @@ function App() {
         <Route 
         path={`/auth`}
         element={<Auth />}
+        />
+        <Route 
+        path={`/card`}
+        element={<MediaCard team1 = "Blue" team2="Purple" 
+        team1runs = {147} team2runs = {152}
+        team1college = "IIT Roorkee" team2college="IIT Kharagpur"
+        toss = "1" matchover = "1" winner ="Blue"
+        team1wickets = "02" team2wickets = "03"
+         />}
         />
       </Routes>
     </div>
